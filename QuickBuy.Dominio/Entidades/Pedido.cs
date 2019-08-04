6bf5395a9022f9a4327d1f0df65219863fd8154b
Jean Silva <jeanalgoritimo@gmail.com>
2019-08-04 19:58:28 -0300

    Novas atualizações.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public ICollection<ItemPedido> ItemPedido { get; set; }
+    }
+}
--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -68,6 +68,8 @@ namespace QuickBuy.Web
 
 
 
+
+
                    
                 }
             });
